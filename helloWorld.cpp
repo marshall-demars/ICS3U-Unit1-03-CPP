8b72f956ec9d0a4605f9974cc8b43fa251b2fc85
Marshall Demars <marshall.demars@mths.ca>
2022-09-16 00:08:52 +0000

    added file

---
+++ helloWorld.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Marshall Demars All rights reserved
+//
+// Created by: Marshall Demars
+// Created on: Sept 2022
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+
+    std::cout << "\nDone.";
+}
